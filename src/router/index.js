b05f8bfd767f610890731dca65322c5e1c5f48f6
lsling <952543325@qq.com>
2017-12-18 10:22:26 +0800

    提交

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+const home = r => require.ensure([], () => r(require('../page/home/home')), 'home')
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
+  	{
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: home
     }
   ]
 })
